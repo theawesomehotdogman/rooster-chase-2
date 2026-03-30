0c083b78296f156fd224eb87c048440c905af39d
Arthur <arthurmarx12345@gmail.com>
2026-03-29 23:14:15 -0500

    added fullscreen and ability to add or remove frame

--- menus/menu.py
+++ menus/menu.py
@@ -6,10 +6,12 @@ def show_text(msg, x, y, color, size):
         fontobj= pygame.font.SysFont("freesans", size,bold=True,italic=False)
         msgobj = fontobj.render(msg,False,color)
         screen.blit(msgobj,(x, y))
-version = "v1.2" #Version number so I can keep track of releases
+version = "v1.2.1" #Version number so I can keep track of releases
 def startscreen(screen,clock):
     gameended = False #Also bad variable name
     mouserect = None
+    fullscreen = False
+    frame = True
     israce = False #Bad variable name but who cares
     startbuttonrect = pygame.Rect(240,220,140,60)
     racebuttonrect = pygame.Rect(240,300,140,60)
@@ -20,6 +22,25 @@ def startscreen(screen,clock):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_F11:
+                    if fullscreen == False:
+                        screen = pygame.display.set_mode((640,480),pygame.FULLSCREEN)
+                        fullscreen = True
+                        break
+                    if fullscreen:
+                        screen = pygame.display.set_mode((640,480))
+                        fullscreen = False
+                        break
+                if event.key == pygame.K_F10:
+                    if frame:
+                        screen = pygame.display.set_mode((640,480),pygame.NOFRAME)
+                        frame = False
+                        break
+                    if frame == False:
+                        screen = pygame.display.set_mode((640,480))
+                        frame = True
+                        break
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mousex,mousey = event.pos
                 mouserect = pygame.Rect(mousex,mousey,32,32)
